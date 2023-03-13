8167b2375be0ef08fd61f1d041457ef619369676
shivangii-sri <shivangisrivastava762@gmail.com>
2023-03-14 01:47:54 +0530

    Removed user/test from MySecurityConfig.java file

--- src/main/java/com/exam/config/MySecurityConfig.java
+++ src/main/java/com/exam/config/MySecurityConfig.java
@@ -71,7 +71,7 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .cors()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/generate-token", "/user/", "/user/test").permitAll()
+                .antMatchers("/generate-token", "/user/").permitAll()
                 .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .anyRequest().authenticated()
                 .and()
